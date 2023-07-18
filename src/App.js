e9b08ddb528c11bb72b418f1a0657c624bfee2aa
shaoor <shaooriqbal786@gmail.com>
2023-07-18 22:46:34 +0500

    react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import Create from './components/Create';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
-   return (
-    <div className="App">
-      <NavBar/>
-      <div className="content">
-       <Home/>
-     </div>
-    </div>
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
-
---
+++ src/components/Create.js
@@ -0,0 +1,12 @@
+const Create = () => {
+    return ( 
+        <div>
+            <h2>Create a Blog</h2>
+        </div>
+     );
+}
+ 
+export default Create;
+<div>
+    <h2>Create a Blog</h2>
+</div>
